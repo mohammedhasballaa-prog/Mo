dd228b470d4bc6dadf009235a4bd2af69e26d5b6
mohammedhasballaa-prog <mohammedhasballaa@gmail.com>
2026-08-24 01:15:14 +0400

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
 plugins {
-    id("com.android.application") version "8.9.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.10" apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+android { namespace="com.mothakrati.app"; compileSdk=35
+    defaultConfig { applicationId="com.mothakrati.app"; minSdk=26; targetSdk=35; versionCode=1; versionName="1.0" }
 }
